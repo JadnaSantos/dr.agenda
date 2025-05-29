ef8a705ab9b7085256f8c09ce8be09fbf161b540
Jadna Santos <jadnajesus7@gmail.com>
2025-05-29 15:17:54 -0300

    feat: table db

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,28 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+import { accountsTable } from "@/db/schema/accouts";
+import { appointmentsTable } from "@/db/schema/appoinments";
+import { clinicsTable } from "@/db/schema/clinics";
+import { doctorsTable } from "@/db/schema/doctors";
+import { patientsTable } from "@/db/schema/patients";
+import { sessionsTable } from "@/db/schema/sessions";
+import { usersTable } from "@/db/schema/users";
+import { usersToClinicsTable } from "@/db/schema/usersToClinics";
+import { verificationsTable } from "@/db/schema/verifications";
+
+export const schema = {
+  usersTable,
+  clinicsTable,
+  doctorsTable,
+  accountsTable,
+  sessionsTable,
+  verificationsTable,
+  appointmentsTable,
+  patientsTable,
+  usersToClinicsTable,
+};
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+});
