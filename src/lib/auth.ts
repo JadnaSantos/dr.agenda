9ce4a130ad2359a1285e99b7d210755b71823e11
Jadna Santos <jadnajesus7@gmail.com>
2025-05-29 15:17:07 -0300

    feat: configs better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,38 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/db";
+import { accountsTable } from "@/db/schema/accouts";
+import { appointmentsTable } from "@/db/schema/appoinments";
+import { clinicsTable } from "@/db/schema/clinics";
+import { doctorsTable } from "@/db/schema/doctors";
+import { patientsTable } from "@/db/schema/patients";
+import { sessionsTable } from "@/db/schema/sessions";
+import { usersTable } from "@/db/schema/users";
+import { usersToClinicsTable } from "@/db/schema/usersToClinics";
+import { verificationsTable } from "@/db/schema/verifications";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     usePlural: true,
+    schema: {
+      usersTable,
+      clinicsTable,
+      doctorsTable,
+      accountsTable,
+      sessionsTable,
+      verificationsTable,
+      appointmentsTable,
+      patientsTable,
+      usersToClinicsTable,
+    },
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   user: {
     modelName: "usersTable",
   },
