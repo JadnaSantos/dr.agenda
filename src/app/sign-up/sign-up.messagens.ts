418b91602efa66129fe5c9ad8dbf3c96d8129cf3
Jadna Santos <jadnajesus7@gmail.com>
2025-05-29 15:19:11 -0300

    feat: seperando as responsabilidade signup

---
+++ src/app/sign-up/sign-up.messagens.ts
@@ -0,0 +1,7 @@
+export const SING_UP_ERROR_MESSAGES = {
+  USER_ALREADY_EXISTS:
+    "E-mail já cadastrado. Tente fazer login ou recupere sua senha.",
+  INVALID_FORM_DATA:
+    "Erro ao criar conta. Verifique se todos os campos estão preenchidos corretamente e tente novamente.",
+  UNKNOWN_ERROR: "Erro ao criar conta. Tente novamente mais tarde.",
+};
---
+++ src/app/sign-up/sign-up.model.ts
@@ -0,0 +1,50 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import type z from "zod";
+
+import { authClient } from "@/lib/auth-client";
+
+import { SING_UP_ERROR_MESSAGES } from "./sign-up.messagens";
+import { SignUpSchema } from "./sign-up.schema";
+
+export const SignUpModel = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof SignUpSchema>) => {
+    await authClient.signUp.email(
+      {
+        name: values.name,
+        email: values.email,
+        password: values.password,
+      },
+      {
+        onSuccess: () => {
+          router.push("/dashboard");
+        },
+        onError: (ctx) => {
+          if (ctx.error.code === SING_UP_ERROR_MESSAGES.USER_ALREADY_EXISTS) {
+            toast.error(SING_UP_ERROR_MESSAGES.USER_ALREADY_EXISTS);
+            return;
+          }
+          toast.error(SING_UP_ERROR_MESSAGES.UNKNOWN_ERROR);
+        },
+      },
+    );
+  };
+
+  return {
+    form,
+    handleSubmit,
+  };
+};
