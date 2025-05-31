0a826102cd71f6a97b16eb85f205f7949a685cd9
Jadna Santos <jadnajesus7@gmail.com>
2025-05-31 20:34:30 -0300

    fix: change way to import table

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,28 +2,9 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-import { accountsTable } from "@/db/schema/accouts";
-import { appointmentsTable } from "@/db/schema/appoinments";
-import { clinicsTable } from "@/db/schema/clinics";
-import { doctorsTable } from "@/db/schema/doctors";
-import { patientsTable } from "@/db/schema/patients";
-import { sessionsTable } from "@/db/schema/sessions";
-import { usersTable } from "@/db/schema/users";
-import { usersToClinicsTable } from "@/db/schema/usersToClinics";
-import { verificationsTable } from "@/db/schema/verifications";
-
-export const schema = {
-  usersTable,
-  clinicsTable,
-  doctorsTable,
-  accountsTable,
-  sessionsTable,
-  verificationsTable,
-  appointmentsTable,
-  patientsTable,
-  usersToClinicsTable,
-};
+import * as schema from "./schema";
+import * as relations from "./schema/usersToCliniRelationsAndClinicsToTableRelations";
 
 export const db = drizzle(process.env.DATABASE_URL!, {
-  schema,
+  schema: { ...schema, ...relations },
 });
