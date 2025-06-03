cce12fc0ab165ee17785358e9cac01d5fcfa90ac
Jadna Santos <jadnajesus7@gmail.com>
2025-06-03 14:25:16 -0300

    refactor: chage prefix

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
-import * as relations from "./schema/usersToCliniRelationsAndClinicsToTableRelations";
+import * as relations from "./schema/relations";
 
 export const db = drizzle(process.env.DATABASE_URL!, {
   schema: { ...schema, ...relations },
