caee6c5887c963b2aa217edec422c7d6f4e0f553
Jadna Santos <jadnajesus7@gmail.com>
2025-06-09 20:26:14 -0300

    style: altera prefix do component

--- src/app/(protected)/appointments/_components/appointment-form/index.tsx
+++ src/app/(protected)/appointments/_components/appointment-form/index.tsx
@@ -2,10 +2,10 @@ import useAppointmentModel from "./appointment.model";
 import type { AddAppointmentFormType } from "./appointment.type";
 import AppointmentView from "./appointment.view";
 
-const AppointmentForm = (props: AddAppointmentFormType) => {
+const AppointmentFormComponent = (props: AddAppointmentFormType) => {
   const methods = useAppointmentModel(props);
 
   return <AppointmentView {...methods} />;
 };
 
-export default AppointmentForm;
+export default AppointmentFormComponent;
