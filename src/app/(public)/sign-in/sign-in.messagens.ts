8c57044b9e3666421ca4b704ac1f4150bea8e97e
Jadna Santos <jadnajesus7@gmail.com>
2025-06-09 18:03:27 -0300

    feat: create custom message for errors sign in and sign up

--- src/app/(public)/sign-in/sign-in.messagens.ts
+++ src/app/(public)/sign-in/sign-in.messagens.ts
@@ -1,5 +1,9 @@
-export const SIGNIN_MESSAGES = {
-  error: {
-    invalid: "Error ao fazer o login",
-  },
+export const SIGN_IN_ERROR_CODES = {
+  USER_NOT_FOUND: "USER_NOT_FOUND",
+  UNKNOWN_ERROR: "UNKNOWN_ERROR",
+} as const;
+
+export const SIGN_IN_ERROR_MESSAGES = {
+  USER_NOT_FOUND: "Usuário não encontrado. Senha ou e-mail incorretos.",
+  UNKNOWN_ERROR: "Erro ao logar. Por favor, Tente novamente mais tarde.",
 };
--- src/app/(public)/sign-up/sign-up.messages.ts
+++ src/app/(public)/sign-up/sign-up.messages.ts
@@ -1,4 +1,10 @@
-export const SING_UP_ERROR_MESSAGES = {
+export const SIGN_UP_ERROR_CODES = {
+  USER_ALREADY_EXISTS: "USER_ALREADY_EXISTS",
+  INVALID_FORM_DATA: "INVALID_FORM_DATA",
+  UNKNOWN_ERROR: "UNKNOWN_ERROR",
+} as const;
+
+export const SIGN_UP_ERROR_MESSAGES = {
   USER_ALREADY_EXISTS:
     "E-mail já cadastrado. Tente fazer login ou recupere sua senha.",
   INVALID_FORM_DATA:
